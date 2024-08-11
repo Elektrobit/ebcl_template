50afe74bf976fbc3478e0a8f8518d544ba2ac150
Tom Irgang <me@tomirgang.de>
2024-08-11 18:26:01 +0000

    improve sbc systemd

--- images/amd64/qemu/ebcl/sbc/systemd/config_root.sh
+++ images/amd64/qemu/ebcl/sbc/systemd/config_root.sh
@@ -14,11 +14,6 @@ ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters
 EOF
 
-# Fix access rights of /etc
-chmod 755 /etc
-chmod -R 755 /etc/systemd
-chmod 600 /etc/ssh/ssh_host_ecdsa_key
-
 # Activate services
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
