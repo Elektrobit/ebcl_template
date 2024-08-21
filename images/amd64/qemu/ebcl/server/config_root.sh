d146a30f17af80f35a8d6a3cfd5a73282c621237
Thomas Irgang (thir820) <thomas.irgang@elektrobit.com>
2024-08-21 16:05:14 +0000

    fix rdb2 image

--- images/amd64/qemu/ebcl/server/config_root.sh
+++ images/amd64/qemu/ebcl/server/config_root.sh
@@ -5,13 +5,6 @@ chmod +x ./sbin/*
 # Create a fake machine-id
 echo "04711" > ./etc/machine-id
 
-# Create a hostname file
-echo "sbc" > ./etc/hostname
-
-# Link DHCP network config
-cd ./etc/crinit/crinit.d
-ln -s ../crinit.net.d/static.crinit
-
 # Create /etc/hosts
 cat >/etc/hosts <<- EOF
 127.0.0.1       localhost
