775001ae9e95c2daffd4c8e49b22a7735698c018
simone-weiss <simone.weiss@elektrobit.com>
2024-11-15 16:56:30 +0100

    Rework build script to work on gh runner
    Signed-off-by: simone-weiss <simone.weiss@elektrobit.com>

--- docs/build_pdf.sh
+++ docs/build_pdf.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-md_files=$(grep -Poz "\(.*md" SUMMARY.md  | tr '(' " "| tr -d '\0')
+cd $(dirname "$0")
+
+index_file=SUMMARY.md
+md_files=$(grep -Poz '\(.*md' "$index_file" |  sed -e "s/(/ /g" | tr -d "\0")
 tex_files=graphics.tex
 file_name=EBcL_SDK.pdf
 
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
+    case "$1" in
         -md) md_files="$2"; shift ;;
         -tex) tex_files="$2"; shift ;;
         -o) file_name="$2"; shift ;;
@@ -14,5 +17,7 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-pandoc $tex_files $md_files -o $file_name --pdf-engine=lualatex --from markdown \
- --template eisvogel --listings --resource-path=assets
+pandoc "$tex_files" $md_files -o "$file_name" --pdf-engine=lualatex --from markdown \
+ --template "./template/eisvogel.tex" --listings --resource-path=assets
+
+cd -
