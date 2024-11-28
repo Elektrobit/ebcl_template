132a632d163b1f656b005c8caf2e4d0e0d409ce2
Andre Barthel <Andre.Barthel@elektrobit.com>
2024-11-28 15:37:32 +0100

    Remodel robot_test to use common resources and have old images their own robot file

--- robot_tests/tools/extract_cases_names.py
+++ robot_tests/tools/extract_cases_names.py
@@ -2,9 +2,13 @@
 # extract_robot_tests.py
 import sys
 from robot.api import TestSuiteBuilder
+from robot.errors import DataError
 
 def get_test_names(robot_file_path):
-    suite = TestSuiteBuilder().build(robot_file_path)
+    try:
+        suite = TestSuiteBuilder().build(robot_file_path)
+    except DataError:
+        return ''
     test_cases = [test.name for test in suite.tests]
     return test_cases
 
