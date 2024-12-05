6aed603ddd2e3760d866dd4396f4f34be3ac76f0
Tom Irgang <thomas@irgang.eu>
2024-12-05 15:54:54 +0100

    add weston and weston service

--- images/arm64/raspberry/pi4/systemd_ui/config_systemd.sh
+++ images/arm64/raspberry/pi4/systemd_ui/config_systemd.sh
@@ -7,3 +7,15 @@ ln -sf /usr/lib/systemd/systemd ./sbin/init
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
 systemctl enable systemd-timesyncd
+systemctl enable weston
+
+systemctl disable systemd-networkd-wait-online
+systemctl disable snapd.seeded
+systemctl disable codemeter
+systemctl disable snapd
+systemctl disable ModemManager
+systemctl disable packagekit
+systemctl disable bluetooth
+systemctl disable wpa_supplicant.service
+systemctl disable networkd-dispatcher.service
+systemctl disable systemd-random-seed
