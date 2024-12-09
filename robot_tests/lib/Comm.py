f18b50e734b826f74425fc4e1f206c4edd69cd4f
Tom Irgang <thomas@irgang.eu>
2024-12-09 15:22:32 +0100

    improve tests

--- robot_tests/lib/Comm.py
+++ robot_tests/lib/Comm.py
@@ -6,7 +6,7 @@ import re
 import time
 
 from subprocess import Popen
-from typing import Optional, Tuple
+from typing import Optional
 from uuid import uuid4
 
 from util.proc_io import ProcIO
@@ -120,7 +120,7 @@ class Comm:
                 logging.info('Match: %s\nFor line: %s', str(m), line)
                 return m
 
-    def execute(self, message, timeout: int = -1) -> str:
+    def execute(self, message, timeout: int = -1, has_echo=True) -> str:
         """
         Read all lines produced by the given message
         """
@@ -136,8 +136,9 @@ class Comm:
         logging.info('Running command %s...', message)
         self.io.write(f'{message}; echo {ter}\n')
 
-        line = self.wait_for_line(ter, timeout=timeout)
-        logging.info('Command echo: %s')
+        if has_echo:
+            line = self.wait_for_line(ter, timeout=timeout)
+            logging.info('Command echo: %s', line)
 
         return self.wait_for_line(ter, timeout=timeout)
 
