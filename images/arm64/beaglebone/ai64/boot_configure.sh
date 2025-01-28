6f72fc0e47f0f458ce7abfa390b82d435388bae9
Thomas Irgang (Elektrobit) <thomas.irgang@elektrobit.com>
2025-01-28 07:12:53 +0000

    fix bb ai64 build and tests

--- images/arm64/beaglebone/ai64/boot_configure.sh
+++ images/arm64/beaglebone/ai64/boot_configure.sh
@@ -6,15 +6,27 @@
 #       extlinux
 #          |------ extlinux.conf
 
+ls -lah
+ls -lah boot/
+ls -lah boot/dtbs/
+
+export KERNEL_VERSION=$(ls boot/dtbs/ | sort | tail -n 1)
+
+echo "Using kernel version: ${KERNEL_VERSION}"
+
 mkdir -p outdir/extlinux
 
 cp opt/u-boot/bb-u-boot-beagleboneai64/microsd-extlinux.conf outdir/extlinux/extlinux.conf
 sed -i 's:init=[^ ]*:init=/usr/bin/init:g' outdir/extlinux/extlinux.conf
-cp boot/dtbs/*/*/k3-j721e-sk.dtb outdir/
-cp boot/dtbs/*/*/k3-j721e-beagleboneai64.dtb outdir/
-cp boot/vmlinuz* outdir/Image.gz
+
+cp boot/dtbs/${KERNEL_VERSION}/*/k3-j721e-sk.dtb outdir/
+cp boot/dtbs/${KERNEL_VERSION}/*/k3-j721e-beagleboneai64.dtb outdir/
+
+cp boot/vmlinuz-${KERNEL_VERSION} outdir/
+mv outdir/vmlinuz* outdir/Image.gz
 gzip -d outdir/Image.gz
 
-cp -r boot/dtbs/*/*/overlays outdir/overlays
+mkdir -p outdir/overlays
 
-cp -r boot/dtbs/*/*/*.dtbo outdir/overlays
+cp -r boot/dtbs/${KERNEL_VERSION}/*/overlays outdir/
+cp -r boot/dtbs/${KERNEL_VERSION}/*/*.dtbo outdir/overlays
