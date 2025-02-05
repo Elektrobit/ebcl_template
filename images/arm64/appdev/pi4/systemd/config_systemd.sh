85ce61ef9a3a822f5f468bbbffc8344dc81250c1
Thomas Irgang (Elektrobit) <thomas.irgang@elektrobit.com>
2025-02-05 08:32:45 +0000

    share common configuration

--- images/arm64/appdev/pi4/systemd/config_systemd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Link systemd as init
-ln -sf /usr/lib/systemd/systemd ./sbin/init
-
-# Activate services
-systemctl enable systemd-networkd
-systemctl enable systemd-resolved
-systemctl enable systemd-timesyncd
