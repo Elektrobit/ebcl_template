fae0b142b6a703a46dfb141446f3cdab1a2c3b73
Thomas Irgang (Elektrobit) <thomas.irgang@elektrobit.com>
2025-02-05 14:51:34 +0000

    test framework parameters from ENV

--- robot_tests/lib/Util.py
+++ robot_tests/lib/Util.py
@@ -2,6 +2,10 @@
 """
 Generic helper functions.
 """
+import logging
+import os
+
+
 class Util:
     """
     Generic helper functions.
@@ -14,3 +18,16 @@ class Util:
         lines.split('\n')
         filtered_lines = [line for line in lines if not search in line]
         return '\n'.join(filtered_lines)
+
+    def set_env(self, variable: str, value: str) -> None:
+        """
+        Set a environment variable
+        """
+        os.environ[variable] = value
+        
+    def get_env(self, variable: str, default: str) -> str:
+        """
+        Get a environment variable
+        """
+        logging.info('Getting environment variable %s...', variable)
+        return os.getenv(variable, default)
