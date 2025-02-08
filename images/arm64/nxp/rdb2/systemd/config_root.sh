9674bc95740df688d016bbd63ea521c813534d8b
Tom Irgang <thomas@irgang.eu>
2025-02-08 17:50:40 +0000

    systemd network test image

--- images/arm64/nxp/rdb2/systemd/config_root.sh
+++ images/arm64/nxp/rdb2/systemd/config_root.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Link systemd as init
-if [ ! -e "./sbin/init"  ]; then
-    ln -s /usr/lib/systemd/systemd ./sbin/init
-else 
-    echo "sbin/init already exists."
-    ls -lah ./sbin/init
-fi
+rm -rf ./sbin/init
+ln -s /usr/lib/systemd/systemd ./sbin/init
+
+# Activate services
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable systemd-timesyncd
