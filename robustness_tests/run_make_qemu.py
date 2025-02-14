a3fc3e958e283c6555119cf6ae7d8b7fc6d33fc6
Mougin, Nicolas <nicolas.mougin@elektrobit.com>
2025-02-14 16:21:44 +0000

    Fix robustness test

--- robustness_tests/run_make_qemu.py
+++ robustness_tests/run_make_qemu.py
@@ -11,7 +11,7 @@ os.chdir(directory)
 # Run build command 10 times in a row
 for _ in range(10):
     print(f"Running build {_+1}...")
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    result = subprocess.run(command, shell=True) #, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 
     if result.returncode != 0:
         print(f"Command failed with return code {result.returncode}")
