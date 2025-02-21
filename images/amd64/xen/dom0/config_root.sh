538e7beb801f251f786999cbf92756ae510cd3de
Thomas Irgang (thir820) <thomas.irgang@elekrobit.com>
2025-02-21 17:02:38 +0000

    DomU EFI image with AMD packages

--- images/amd64/xen/dom0/config_root.sh
+++ images/amd64/xen/dom0/config_root.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 # Link systemd as init
-ln -s /usr/lib/systemd/systemd /sbin/init
+rm -f ./sbin/init
+ln -sf /usr/lib/systemd/systemd ./sbin/init
+
+# Link resolv.conf from systemd-resolved
+rm -f ./etc/resolv.conf
+ln -sf /run/systemd/resolve/stub-resolv.conf ./etc/resolv.conf
+
+# Activate services
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable systemd-timesyncd
 
 # Start ebcl VM
 systemctl enable start-ebcl-1.service
--- images/amd64/xen/domu/config_root.sh
+++ images/amd64/xen/domu/config_root.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-# Link crinit as init
+# Link systemd as init
 rm -f ./sbin/init
-ln -s /usr/bin/crinit ./sbin/init
+ln -sf /usr/lib/systemd/systemd ./sbin/init
 
+# Activate services
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
+systemctl enable systemd-timesyncd
+systemctl enable symlink-resolvconf
+
+# Setup locale
+RUN export LC_ALL=en_US.UTF-8 && \
+    export LANG=en_US.UTF-8 && \
+    locale-gen en_US.UTF-8
+
+ls -lah /dev/*
+
+# Install AMD stuff
 apt update
 cd /amd
 chmod +x install_debs.sh
-export DEBIAN_FRONTEND="noninteractive"
-export TZ="Etc/UTC"
-yes | ./install_debs.sh
