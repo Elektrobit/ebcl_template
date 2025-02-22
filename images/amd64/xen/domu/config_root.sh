a0f81cabe40f012c6151b4c30d49ee3aa1de9856
Thomas Irgang (thir820) <thomas.irgang@elekrobit.com>
2025-02-22 10:20:06 +0000

    domu grub efi boot

--- images/amd64/xen/domu/config_root.sh
+++ images/amd64/xen/domu/config_root.sh
@@ -11,9 +11,9 @@ systemctl enable systemd-timesyncd
 systemctl enable symlink-resolvconf
 
 # Setup locale
-RUN export LC_ALL=en_US.UTF-8 && \
-    export LANG=en_US.UTF-8 && \
-    locale-gen en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale-gen en_US.UTF-8
 
 ls -lah /dev/*
 
