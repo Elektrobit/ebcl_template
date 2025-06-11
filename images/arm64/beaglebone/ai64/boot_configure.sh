67f86d53bad3236132e79f329ff5d9ee868c5fe6
anpa8480 <andor.pastor@gmail.com>
2025-06-11 15:51:23 +0200

    Update images/arm64/beaglebone/ai64/boot_configure.sh
    Signed-off-by: anpa8480 <andor.pastor@gmail.com>

--- images/arm64/beaglebone/ai64/boot_configure.sh
+++ images/arm64/beaglebone/ai64/boot_configure.sh
@@ -19,8 +19,8 @@ mkdir -p outdir/extlinux
 cp opt/u-boot/bb-u-boot-beagleboneai64/microsd-extlinux.conf outdir/extlinux/extlinux.conf
 sed -i 's:init=[^ ]*:init=/usr/bin/init:g' outdir/extlinux/extlinux.conf
 
-cp boot/dtbs/${KERNEL_VERSION}/*/k3-j721e-sk.dtb outdir/ # Y
-cp boot/dtbs/${KERNEL_VERSION}/*/k3-j721e-beagleboneai64.dtb outdir/ # Y 
+cp boot/dtbs/${KERNEL_VERSION}/*/k3-j721e-sk.dtb outdir/
+cp boot/dtbs/${KERNEL_VERSION}/*/k3-j721e-beagleboneai64.dtb outdir/
 
 cp boot/vmlinuz-${KERNEL_VERSION} outdir/
 mv outdir/vmlinuz* outdir/Image.gz
